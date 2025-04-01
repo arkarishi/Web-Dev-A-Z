44a7d61d3fe840d0dbe39da17b196d85a1614e15
arkarishi <arkaprovo.rishi@gmail.com>
2025-04-01 21:43:47 +0530

    Update

---
+++ VIDEO 63/mfr.js
@@ -0,0 +1,24 @@
+let arr=[1,13,5,7,11]
+
+let newArr=[]
+
+// for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+//     newArr.push(element**2)
+// }
+
+// console.log(newArr)
+
+newArr=arr.map((e,index,arr)=>{
+    return e**2
+})
+console.log(newArr)
+
+const greaterThanSeven = (e)=>{
+    if (e>7) {
+        return true
+    }
+    return false
+}
+
+console.log(arr.filter(greaterThanSeven))
